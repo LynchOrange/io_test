f5d0a2d96583bef9bc8ebd4e0cc8a806dd2f3560
LynchOrange <yuluoljl@gmail.com>
2021-04-22 12:07:30 -0500

    first commit

---
+++ src/iodata/testcase1.java
@@ -0,0 +1,20 @@
+package iodata;
+
+public class testcase1 {
+	
+	public static int add(int a, int b) {
+		return a + b;
+		
+	}
+	
+	public static int mul(int a, int b) {
+		return a * b;
+		
+	}
+	
+	public static int sub(int a, int b) {
+		return a - b;
+		
+	}
+
+}
